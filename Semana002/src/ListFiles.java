5c0eb8f7fcd5d0b8b4bef27c7d194df3fbead843
LP <pinto.filipe.luis@gmail.com>
2024-09-30 16:32:39 +0100

    Update ListFiles class

--- Semana002/src/ListFiles.java
+++ Semana002/src/ListFiles.java
@@ -3,41 +3,64 @@ import java.util.ArrayList;
 import java.util.List;
 import static java.lang.System.out;
 
+// 2.6a listar todos os ficheiros de um diretório
 public class ListFiles {
+
+	// lista os ficheiros
 	public static List<String> listarArquivos(String diretorio) {
-		File folder = new File(diretorio);
+		String execPath = System.getProperty("user.dir");
+		File folder = new File(execPath);
 		File[] listOfFiles = folder.listFiles();
 		List<String> arquivos = new ArrayList<>();
-		
-		out.println(listOfFiles.length);
-		
+
 		if (listOfFiles != null) {
-			
-			out.println("entrou aqui");
-			
 			for (File file : listOfFiles) {
 				if (file.isFile()) {
-					arquivos.add(file.getAbsolutePath());
+					arquivos.add(file.getName());
 				}
 			}
-		}
-		for (String s : arquivos)
-			out.println(s);	
+		}	
 		return arquivos;
 	}
 
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.out.println("Uso: java ListFiles <diretorio>");
-			return;
-		}
-
-		String diretorio = args[0];
-		List<String> arquivos = listarArquivos(diretorio);
-
-		for (String arquivo : arquivos) {
-			System.out.println(arquivo);
-		}
+	// função recursiva
+	public static ArrayList<File> collectFiles(File dir) {
+		ArrayList<File> list = new ArrayList<>();
+		collectFilesRec(dir, list);
+		return list;
 	}
 
+	private static void collectFilesRec(File f, List<File> list) {
+        if (f.isDirectory()) {
+            File[] files = f.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    collectFilesRec(file, list);
+                }
+            }
+        } else {
+            list.add(f);
+        }
+    }
+
+	public static void main(String[] args) {
+		String temp = args[0];
+		if (temp.equals("-r")){
+			//executa a função recursiva
+			String diretorio = args[1];
+			File f = new File(System.getProperty("user.dir"));
+			List<File> arquivos = collectFiles(f);
+			for (File arquivo : arquivos) {
+				out.println(diretorio + "/" + arquivo.getAbsolutePath());
+			}
+		}
+		else{
+			String diretorio = args[0];
+			List<String> arquivos = listarArquivos(diretorio);
+			for (String arquivo : arquivos) {
+				out.println(diretorio + "/" + arquivo);
+			}
+
+		}
+	}
 }
